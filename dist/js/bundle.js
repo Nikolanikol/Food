b9418cdd1308591b633129a009c6d9ad45a5572f
Nikolai <krezi3927@gmail.com>
2023-08-17 20:57:19 +0900

    css adaptive

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -613,42 +613,41 @@ function tabs (tabsSelector, tabsContentselector, tabsParentSelector, activeClas
     const tabHeaderItem = document.querySelectorAll(tabsSelector),
           tabContent = document.querySelectorAll(tabsContentselector),
           tabHeaderItems = document.querySelector(tabsParentSelector);
-          console.log(`tabheaderItem ${tabHeaderItem}`)
-          console.log(`tabContent${tabContent}`)
-          console.log(`tabHeaderItems$${tabHeaderItems}`)
-function tabContentHidden() {
-    tabContent.forEach((item) => {
-        item.classList.add('hidden');
-    })
-}
-function tabContentHiddenRemove(i = 0) {
-    tabContent[i].classList.remove('hidden');
-}
-function tabContentShow(i) {
-    tabContent.forEach(item => {
-        item.classList.remove('show');
-    })
-    tabContent[i].classList.add('show');
-}
-tabContentHidden();
-tabContentHiddenRemove();
+     
 
-tabHeaderItems.addEventListener('click', (event) => {
-    console.log('tabs click')
-    if (event.target.className == tabsSelector.slice(1)) {
-        tabHeaderItem.forEach((item) => {
-            item.classList.remove(activeClass)
-            tabContentHidden();
-        });
-        event.target.classList.add(activeClass);
-
+    function tabContentHidden() {
+        tabContent.forEach((item) => {
+            item.classList.add('hidden');
+        })
     }
-    tabHeaderItem.forEach((item, i) => {
-        if (item.classList.contains(activeClass)) {
-            tabContentHiddenRemove(i);
+    //функция для отмены скрытия слайда. значение по умолчанию 0
+    function tabContentHiddenRemove(i = 0) {
+        tabContent[i].classList.remove('hidden');
+    }
+
+    function tabContentShow(i) {
+        tabContent.forEach(item => {
+            item.classList.remove('show');
+        })
+        tabContent[i].classList.add('show');
+    }
+    tabContentHidden();
+    tabContentHiddenRemove();
+
+    tabHeaderItems.addEventListener('click', (event) => {
+        if (event.target.className == tabsSelector.slice(1)) {
+            tabHeaderItem.forEach((item) => {
+                item.classList.remove(activeClass)
+                tabContentHidden();
+            });
+            event.target.classList.add(activeClass);
         }
+        tabHeaderItem.forEach((item, i) => {
+            if (item.classList.contains(activeClass)) {
+                tabContentHiddenRemove(i);
+            }
+        })
     })
-})
 }
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (tabs);
 
